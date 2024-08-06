884d6c461c84e675c32710947a7564ae89e89176
leocklaus <leocklaus@gmail.com>
2024-08-06 15:07:24 -0300

    feat: cart icon and quantity on navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
+import { CartService } from '../shared/services/cart.service';
+import { RouterModule } from '@angular/router';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, MatIconModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+
+  cartService = inject(CartService);
+  cartQty = computed(()=> {
+    console.log("computou")
+    return this.cartService.cart().reduce((acc, item)=> acc + item.quantity, 0)
+  })
 
 }
